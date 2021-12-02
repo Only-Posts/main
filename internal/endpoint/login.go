4625e3797587a04baefa0177479d14144812ff8c
P4NUTA <PaulEfimov2001@yandex.ru>
2021-12-03 01:01:41 +0300

    [BACKEND-18] Make login

--- internal/endpoint/login.go
+++ internal/endpoint/login.go
@@ -1,7 +1,48 @@
 package endpoint
 
-import "github.com/gofiber/fiber"
+import (
+	"log"
+
+	"github.com/gofiber/fiber"
+	"only-post-api/internal/auth"
+	"only-post-api/internal/repository"
+)
 
 func PostLogin(ctx *fiber.Ctx) {
-	ctx.Send("HELLO LOGIN!")
+	user := new(auth.UserData)
+	err := ctx.BodyParser(user)
+	if err != nil {
+		ctx.JSON(fiber.Map{"status": "failed", "message": "failed to parse body"})
+		log.Printf("Error parse signup message err: %s\n", err)
+		return
+	}
+
+	getuser, err := repository.GetUserByEmail(user.Email)
+	if err != nil {
+		return
+	}
+	if getuser.Email != "" {
+		if getuser.Password != user.Password {
+			ctx.JSON(fiber.Map{"status": "failed", "message": "wrong password"})
+			return
+		}
+		ctx.JSON(fiber.Map{"status": "success", "message": "user login", "user_id": getuser.ID})
+		return
+	}
+
+	getuser, err = repository.GetUserByUsername(user.Username)
+	if err != nil {
+		return
+	}
+	if getuser.Username != "" {
+		if getuser.Password != user.Password {
+			ctx.JSON(fiber.Map{"status": "failed", "message": "wrong password"})
+			return
+		}
+		ctx.JSON(fiber.Map{"status": "success", "message": "user login", "user_id": getuser.ID})
+		return
+	}
+
+	ctx.JSON(fiber.Map{"status": "failed", "message": "user not found"})
+	return
 }
--- internal/endpoint/signup.go
+++ internal/endpoint/signup.go
@@ -1,16 +1,53 @@
 package endpoint
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber"
 	"only-post-api/internal/auth"
+	"only-post-api/internal/repository"
 )
 
 func PostSignUp(ctx *fiber.Ctx) {
-	var user *auth.UserData
+	user := new(auth.UserData)
 	err := ctx.BodyParser(user)
 	if err != nil {
 		ctx.JSON(fiber.Map{"status": "failed", "message": "failed to parse body"})
+		log.Printf("Error parse signup message err: %s\n", err)
 		return
 	}
-	ctx.JSON(fiber.Map{"status": "success", "message": "user registered"})
+
+	getuser, err := repository.GetUserByEmail(user.Email)
+	if err != nil {
+		return
+	}
+	if getuser.Email != "" {
+		if getuser.Password != user.Password {
+			ctx.JSON(fiber.Map{"status": "failed", "message": "wrong password"})
+			return
+		}
+		ctx.JSON(fiber.Map{"status": "success", "message": "user already registered"})
+		return
+	}
+
+	getuser, err = repository.GetUserByUsername(user.Username)
+	if err != nil {
+		return
+	}
+	if getuser.Username != "" {
+		if getuser.Password != user.Password {
+			ctx.JSON(fiber.Map{"status": "failed", "message": "wrong password"})
+			return
+		}
+		ctx.JSON(fiber.Map{"status": "success", "message": "user already registered"})
+		return
+	}
+
+	err = repository.RegisterUser(user)
+	if err != nil {
+		ctx.JSON(fiber.Map{"status": "failed", "message": "failed to register"})
+		return
+	}
+	ctx.JSON(fiber.Map{"status": "success", "message": "user registered", "user_id": user.ID})
+	return
 }
