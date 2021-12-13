852ab9eaae604cc0bf6e028148ab10b57d35ca13
P4NUTA <PaulEfimov2001@yandex.ru>
2021-12-13 23:28:48 +0300

    [DEVOPS-22] Docker

--- init.sql
+++ init.sql
@@ -1,3 +1,4 @@
-CREATE USER postgres;
+CREATE USER postgres PASSWORD 'postgres';
 CREATE DATABASE postgres;
 GRANT ALL PRIVILEGES ON DATABASE postgres TO postgres;
+ALTER USER postgres PASSWORD 'postgres';
